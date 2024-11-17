d890570f7a854ba6e366c20d3f7e2fea5b398f23
rafa <rafagomezguillen03@gmail.com>
2024-11-17 11:09:50 +0000

    Enhance Header component with dropdown menu and updated branding

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import "./Header.css";
+
+// Import bootstrap components
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 export const Header = () => {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">Playground</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-      </Container>
+    <Navbar style={{ padding: "10px" }}>
+      <Navbar.Brand href="#home">Playground sin mas</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="#home">Home</Nav.Link>
+        <NavDropdown
+          id="nav-dropdown-dark-example"
+          title="Modes"
+          menuVariant="dark"
+        >
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
     </Navbar>
   );
 };
